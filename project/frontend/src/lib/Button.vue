73870ecfbc98d4c40aeaf4bcd299140f8498235a
S1lence-z <j3.zelenka@gmail.com>
2025-07-12 11:45:17 +0200

    feat: add button component

---
+++ project/frontend/src/lib/Button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+	label: string;
+	type: string | "button";
+	buttonStyle: string | "primary" | "secondary";
+	onclick?: () => void;
+	disabled: boolean | false;
+}>();
+</script>
+
+<template>
+	<button class="btn" :class="`btn-${props.buttonStyle}`" :disabled="props.disabled" @click="props.onclick">
+		{{ props.label }}
+	</button>
+</template>
