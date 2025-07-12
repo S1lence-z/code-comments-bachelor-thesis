8b4528ef5e5487238213301878b26d7e716bf6f7
S1lence-z <j3.zelenka@gmail.com>
2025-07-13 00:18:53 +0200

    fix: fix padding

--- project/frontend/src/components/FileExplorer.vue
+++ project/frontend/src/components/FileExplorer.vue
@@ -19,7 +19,7 @@ defineEmits<{
 		class="h-full flex flex-col bg-[#252526] text-[#cccccc] overflow-y-auto select-none border-r border-[#2d2d30] font-sans min-w-[200px]"
 	>
 		<div
-			class="px-4 py-2.5 pl-5 text-xs font-bold text-[#cccccc] uppercase tracking-widest bg-[#2d2d30] border-b border-[#3c3c3c] sticky top-0 z-10 flex items-center h-10 box-border before:content-[''] before:w-4 before:h-4 before:mr-1.5 before:bg-[url('data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 16 16\'%3E%3Cpath fill=\'%23cccccc\' d=\'M14.5 3H7.71l-.85-.85L6.51 2h-5a.5.5 0 0 0-.5.5v11a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-10a.5.5 0 0 0-.5-.5z\'/%3E%3C/svg%3E')] before:bg-no-repeat before:bg-contain before:flex-shrink-0"
+			class="	py-2.5 text-xs font-bold text-[#cccccc] uppercase tracking-widest bg-[#2d2d30] border-b border-[#3c3c3c] sticky top-0 z-10 flex items-center h-10 box-border before:content-[''] before:w-4 before:h-4 before:mr-1.5 before:bg-[url('data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 16 16\'%3E%3Cpath fill=\'%23cccccc\' d=\'M14.5 3H7.71l-.85-.85L6.51 2h-5a.5.5 0 0 0-.5.5v11a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-10a.5.5 0 0 0-.5-.5z\'/%3E%3C/svg%3E')] before:bg-no-repeat before:bg-contain before:flex-shrink-0"
 		>
 			Explorer
 		</div>
