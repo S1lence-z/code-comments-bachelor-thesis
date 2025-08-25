45d25ca16d1421681a7acffdd923d8a25d686a0b
S1lence-z <j3.zelenka@gmail.com>
2025-08-24 21:07:27 -0400

    chore: remove the express.js backend

--- project/backend/src/controllers/categoriesController.ts
+++
@@ -1,8 +0,0 @@
-import { CategoryDto } from "../models/dtoModels";
-import CategoriesService from "../services/categoriesService";
-
-export default class CategoriesController {
-	public static getCategories(categoriesService: CategoriesService): CategoryDto[] {
-		return categoriesService.getCategories();
-	}
-}
