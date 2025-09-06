1fdcd86644fa02eb73f39d3edd509274d47ffc95
S1lence-z <j3.zelenka@gmail.com>
2025-09-06 19:33:20 -0400

    fix: add base to router

--- frontend/src/core/router.ts
+++ frontend/src/core/router.ts
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter } from "vue-router";
 import routes from "./routes.ts";
 
 export const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHistory("/code-comments-bachelor-thesis/"),
 	routes,
 });
 
