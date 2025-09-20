7cafd538e9a4bf905720805e8ad027d7c84f07b0
S1lence-z <j3.zelenka@gmail.com>
2025-09-20 22:09:38 +0100

    feat: refactor github file service

--- frontend/src/services/githubFileService.ts
+++ frontend/src/services/githubFileService.ts
@@ -1,61 +1,69 @@
 import type { ProcessedFile, GithubFileContentResponse } from "../types/github/githubFile";
 import { FileDisplayType } from "../types/github/githubFile";
 
-export async function fetchProcessedFile(
-	repositoryUrl: string,
-	branch: string,
-	path: string,
-	GITHUB_PAT?: string
-): Promise<ProcessedFile> {
-	const url = new URL(repositoryUrl);
-	const [owner, repo] = url.pathname.split("/").filter(Boolean);
-	if (!owner || !repo) {
-		throw new Error("Invalid repository URL for API construction.");
-	}
-	const contentUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branch}`;
+const useGithubFileService = () => {
+	const processFileContent = (file: GithubFileContentResponse): ProcessedFile => {
+		const { name, content, download_url } = file;
+		let displayType: FileDisplayType = FileDisplayType.Text;
+		let fileContent: string | null = null;
 
-	const headers: HeadersInit = {
-		Accept: "application/vnd.github+json",
-	};
-	if (GITHUB_PAT) {
-		headers["Authorization"] = `Bearer ${GITHUB_PAT}`;
-	}
-
-	const response = await fetch(contentUrl, { headers });
-	if (!response.ok) {
-		const errorText = await response.json();
-		throw new Error(`Status ${response.status}: ${errorText || "Failed to load file content."}`);
-	}
-	const data: GithubFileContentResponse = await response.json();
-	const processedFile = processFileContent(data);
-	return processedFile;
-}
-
-function processFileContent(file: GithubFileContentResponse): ProcessedFile {
-	const { name, content, download_url } = file;
-	let displayType: FileDisplayType = FileDisplayType.Text;
-	let fileContent: string | null = null;
-
-	if (name.endsWith(".jpg") || name.endsWith(".png")) {
-		displayType = FileDisplayType.Image;
-	} else if (name.endsWith(".pdf")) {
-		displayType = FileDisplayType.PDF;
-	} else if (!content) {
-		displayType = FileDisplayType.Binary;
-	} else {
-		// Github API returns base64 encoded content for text files
-		fileContent = atob(content);
-		const bytes = new Uint8Array(fileContent.length);
-		for (let i = 0; i < fileContent.length; i++) {
-			bytes[i] = fileContent.charCodeAt(i);
+		if (name.endsWith(".jpg") || name.endsWith(".png")) {
+			displayType = FileDisplayType.Image;
+		} else if (name.endsWith(".pdf")) {
+			displayType = FileDisplayType.PDF;
+		} else if (!content) {
+			displayType = FileDisplayType.Binary;
+		} else {
+			// Github API returns base64 encoded content for text files
+			fileContent = atob(content);
+			const bytes = new Uint8Array(fileContent.length);
+			for (let i = 0; i < fileContent.length; i++) {
+				bytes[i] = fileContent.charCodeAt(i);
+			}
+			fileContent = new TextDecoder("utf-8").decode(bytes);
 		}
-		fileContent = new TextDecoder("utf-8").decode(bytes);
-	}
 
-	return {
-		displayType,
-		content: fileContent,
-		downloadUrl: download_url,
-		fileName: name,
+		return {
+			displayType,
+			content: fileContent,
+			downloadUrl: download_url,
+			fileName: name,
+		};
 	};
-}
+
+	const fetchProcessedFile = async (
+		repositoryUrl: string,
+		branch: string,
+		path: string,
+		githubPat?: string
+	): Promise<ProcessedFile> => {
+		// Validate and parse the repository URL
+		const url = new URL(repositoryUrl);
+		const [owner, repo] = url.pathname.split("/").filter(Boolean);
+		if (!owner || !repo) {
+			throw new Error("Invalid github repository URL for API construction.");
+		}
+
+		// Construct the API call
+		const contentUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branch}`;
+		const headers: HeadersInit = {
+			Accept: "application/vnd.github+json",
+		};
+		if (githubPat) {
+			headers["Authorization"] = `Bearer ${githubPat}`;
+		}
+
+		// Fetch the file content
+		const response = await fetch(contentUrl, { headers });
+		if (!response.ok) {
+			const errorText = await response.json();
+			throw new Error(`Failed to load file content for ${path}. Error ${response.status}: ${errorText || ""}`);
+		}
+		const responseData: GithubFileContentResponse = await response.json();
+		const processedFile = processFileContent(responseData);
+		return processedFile;
+	};
+	return { fetchProcessedFile };
+};
+
+export default useGithubFileService;
