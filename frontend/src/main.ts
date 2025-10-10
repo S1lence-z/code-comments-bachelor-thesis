b2d7f09d57ad27f34e0da865218fdca551a3ffc6
S1lence-z <j3.zelenka@gmail.com>
2025-10-10 14:23:47 +0100

    feat: add i18n and en.json locale

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,5 +5,15 @@ import App from "./App.vue";
 import { createPinia } from "pinia";
 import "./../css/tailwind.css";
 import { Icon } from "@iconify/vue";
+import { createI18n } from "vue-i18n";
+import en from "./locales/en.json";
 
-createApp(App).use(createPinia()).use(router).component("Icon", Icon).mount("#app");
+const i18n = createI18n({
+	locale: "en",
+	fallbackLocale: "en",
+	messages: {
+		en,
+	},
+});
+
+createApp(App).use(createPinia()).use(router).component("Icon", Icon).use(i18n).mount("#app");
