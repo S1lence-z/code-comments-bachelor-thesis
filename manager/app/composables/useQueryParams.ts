55d3b73f2717c7aa98dac961f3b771d26d1255dd
S1lence-z <j3.zelenka@gmail.com>
2025-10-12 22:45:34 +0100

    fix: add query param types to composable

--- manager/app/composables/useQueryParams.ts
+++ manager/app/composables/useQueryParams.ts
@@ -1,4 +1,18 @@
-import { QUERY_PARAMS } from "../../shared/types/QueryParams";
+export const QUERY_PARAMS = {
+	SERVER_BASE_URL: "serverBaseUrl",
+	REPOSITORY_URL: "repositoryUrl",
+	RW_API_URL: "rwApiUrl",
+	BRANCH: "branch",
+	FILE: "file",
+};
+
+export interface QueryParams {
+	serverBaseUrl?: string;
+	repositoryUrl?: string;
+	rwApiUrl?: string;
+	branch?: string;
+	file?: string;
+}
 
 export const useQueryParams = () => {
 	const config = useRuntimeConfig();
