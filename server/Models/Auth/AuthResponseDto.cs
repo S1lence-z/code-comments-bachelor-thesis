9f7c309082259209da0d6aca6d49d67a62eab11c
S1lence-z <j3.zelenka@gmail.com>
2025-11-25 23:37:34 +0000

    feat: add jwt settings and models

---
+++ server/Models/Auth/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace server.Models.Auth
+{
+    public class AuthResponseDto
+    {
+        public bool Success => !string.IsNullOrEmpty(Token);
+        public string Message { get; set; } = string.Empty;
+		public string? Token { get; set; } = string.Empty;
+    }
+}
---
+++ server/Models/Auth/LoginRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Auth
+{
+    public class LoginRequestDto
+    {
+		[Required]
+		public string Password { get; set; } = string.Empty;
+	}
+}
