2e131e3fe10f0739ff1b6a18779169f58c0669b8
S1lence-z <j3.zelenka@gmail.com>
2025-12-01 15:59:03 +0000

    fix: remove tailwindcss deps from base

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -1,11 +1,6 @@
-import tailwindcss from "@tailwindcss/vite";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: false },
 	srcDir: "app/",
 	modules: ["@nuxt/icon", "@pinia/nuxt"],
-	vite: {
-		plugins: [tailwindcss()],
-	},
 });
