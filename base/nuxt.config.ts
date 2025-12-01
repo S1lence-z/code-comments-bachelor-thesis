8c5da104e88557c82e752545f8b05a084804101a
S1lence-z <j3.zelenka@gmail.com>
2025-12-01 15:25:36 +0000

    chore: add tailwindcss to base deps

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -1,6 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  srcDir: "app/",
-  modules: ["@nuxt/icon", "@pinia/nuxt"],
+	devtools: { enabled: false },
+	srcDir: "app/",
+	modules: ["@nuxt/icon", "@pinia/nuxt"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
